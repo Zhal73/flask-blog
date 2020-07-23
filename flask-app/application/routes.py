2406884dace673bf65839c139ee0d640ab1746d7
zhal73 <domenico.gagliano1973@gmail.com>
2020-07-23 14:36:21 +0000

    fixed multiple '/' in routes.py

--- flask-app/application/routes.py
+++ flask-app/application/routes.py
@@ -22,17 +22,14 @@ blogData = [
 def home():
     return render_template('home.html', title='Home', posts=blogData)
 
-@app.route('/')
 @app.route('/about')
 def about():
     return render_template('about.html',title='About')
 
-@app.route('/')
 @app.route('/login')
 def login():
     return render_template('login.html',title='Login')
 
-@app.route('/')
 @app.route('/register')
 def register():
     return render_template('register.html',title='Register')
