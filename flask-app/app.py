6801045fd4a5d42a95da29f025faf9b2435391fa
zhal73 <domenico.gagliano1973@gmail.com>
2020-07-23 12:32:02 +0000

    Flask templates exercise

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,28 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-@app.route('/home')
-def home():
-    return """
-        "Hello internet!!!!"
-        <p>
-        <a href="../example">Example</a>
-        </p>
-        """
-@app.route('/')
-@app.route('/example')
-def example():
-    return """
-        <!DOCTYPE html>
-        <html>
-        <body>
-
-        <h1>My First Heading</h1>
-        <p>My first paragraph.</p>
-        <p>I am very good at this!!! (not)</p>
-        <a href="../home">Home </a>
-        </body>
-        </html>
-        """
+# imports app object from ./application/__init__.py
+from application import app
+# if the file is being run directly, and not imported, then start the application.
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
---
+++ flask-app/application/routes.py
@@ -0,0 +1,24 @@
+# import render_template function from the flask module
+from flask import render_template
+# import the app object from the ./application/__init__.py
+from application import app
+# define routes for / & /home, this function will be called when these are accessed
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', title='Home')
+
+@app.route('/')
+@app.route('/about')
+def about():
+    return render_template('about.html',title='About')
+
+@app.route('/')
+@app.route('/login')
+def login():
+    return render_template('login.html',title='Login')
+
+@app.route('/')
+@app.route('/register')
+def register():
+    return render_template('register.html',title='Register')
