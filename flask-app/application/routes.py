206ccd134d810a99250381917860b56e5c6e95df
zhal73 <domenico.gagliano1973@gmail.com>
2020-07-24 15:00:26 +0000

    add post.html and secret key management

--- flask-app/application/routes.py
+++ flask-app/application/routes.py
@@ -1,11 +1,8 @@
-# import render_template function from the flask module
-from flask import render_template
-# import the app object from the ./application/__init__.py
-from application import app
-
+from flask import render_template, redirect, url_for
+from application import app, db
 from application.models import Posts
+from application.forms import PostForm 
 
-# define routes for / & /home, this function will be called when these are accessed
 @app.route('/')
 @app.route('/home')
 def home():
@@ -23,3 +20,24 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html',title='Register')
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        postData = Posts(
+            first_name = form.first_name.data,
+            last_name = form.last_name.data,
+            title = form.title.data,
+            content = form.content.data
+        )
+
+        db.session.add(postData)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    else:
+        print(form.errors)
+
+    return render_template('post.html', title='Post', form=form)
