44db9313085281d8045e69423d95fc4a9d799dc4
zhal73 <domenico.gagliano1973@gmail.com>
2020-07-30 16:25:16 +0000

    test coverage 95%

--- flask-app/tests/test_back_end.py
+++ flask-app/tests/test_back_end.py
@@ -12,7 +12,7 @@ from os import getenv
 # setup configuration so that the tests can be run
 # and the test that will be performed
 class TestBase(TestCase):
-
+    pass
     def create_app(self):
         # pass in configurations for test database
         # this allows the test suite to connect to the database
@@ -53,22 +53,22 @@ class TestBase(TestCase):
         # posts that can cause proble to the test performed.
         db.session.remove()
         db.drop_all()
-
 ##### ACTUAL TESTS ########
 class TestViews(TestBase):
-
+    pass
     def test_homepage_view(self):
         # this tests if the homepage is accessible
         # by anyone, without the needs for a
         # user to be logged in
-
+        # tests line 10,13 and 14 of routes.py
         response = self.client.get(url_for('home')) #call the home page
         self.assertEqual(response.status_code, 200) #checks if it is accessible. code : 200
-
+    
     def test_add_new_post(self):
         # this tests that once a user posts a new post,
         # the user is then redirected to the home page
         # and the new post is visible
+        # test line 11-12,23-36,66,68-74, 76 in routes.py
         with self.client:
             # first login the user
             self.client.post(url_for('login'), data = dict(email = "admin@admin.com", password="admin2016"), follow_redirects = True)
@@ -83,33 +83,37 @@ class TestViews(TestBase):
                 follow_redirects=True   # follows the redirecton
             )
             self.assertIn(b'Test Title', response.data) # Checks it the "Test Title" we posted is in the home page
-
+    
     def test_about_page_view(self):
         # this tests that the abouit page can be
         # seen by anyone without the needs
         # to be logged in
+        # test line 18 in routes.py
         response = self.client.get(url_for('about')) #calls the about page
         self.assertEqual(response.status_code, 200) #checks if it is accessible, code: 200
-
+    
     def test_post_page_access_not_log(self):
         # this test that if a not logged-in
         # uset wants to access post
         # is sent to the login page
+        # no chenges in statistics
         response = self.client.get(url_for('post')) #call the post page
         self.assertIn(b'login', response.data) # Checks if the we get the login page
-
+    
     def test_post_page_access_logged(self):
         # this test that if a not logged-in
         # uset wants to access post
         # is sent to the login page
+        # tests 37-39 in routes.py
         self.client.post(url_for('login'), data = dict(email = "admin@admin.com", password="admin2016"), follow_redirects = True)
 
         response = self.client.get(url_for('post')) #call the post page
         self.assertIn(b'Post', response.data) # Checks if get the post page
-
+    
     def test_register(self):
         # this test checks if is possible to 
         # register as new user
+        # tests 43, 45-62 in routes.py
         with self.client:
             resp=self.client.post(url_for('register'),data = dict(first_name="domen", last_name="gagli", email="dddd@gmail.com", password="1234",confirm_password="1234"),follow_redirects = True)
             #self.assertRedirects(resp.data,url_for('login'))
@@ -117,7 +121,7 @@ class TestViews(TestBase):
 
     def test_account(self):
         # test it the account page is loaded correctly
-
+        # tests 91-93, 99-104 in routes.py
         # first login the user
         self.client.post(url_for('login'), data = dict(email = "admin@admin.com", password="admin2016"), follow_redirects = True)
         response = self.client.get(url_for('account'))
@@ -128,6 +132,7 @@ class TestViews(TestBase):
         # by changing the first
         # the user is redirected to the home page
         # and the new name appears in the homepage
+        # TESTS 94-98 IN ROUTES.PY
 
         # first login the user
         self.client.post(url_for('login'), data = dict(email = "admin@admin.com", password="admin2016"), follow_redirects = True)
@@ -138,7 +143,16 @@ class TestViews(TestBase):
     def test_logout(self):
         # this tests if after logged out
         # a the user is redirect to the login page
+        # tests 84-85 in routes.py
         self.client.post(url_for('login'), data = dict(email = "admin@admin.com", password="admin2016"), follow_redirects = True)
         rv = self.client.get('logout',follow_redirects=True)
         self.assertIn(b'Login Page', rv.data)
+    
+    def test_account_delete(self):
+        # tests the successful 
+        # account deletion
+        # tests 110-115, 117-121 in routes.py
+        self.client.post(url_for('login'), data = dict(email = "admin@admin.com", password="admin2016"), follow_redirects = True)
+        rv = self.client.get('account/delete',follow_redirects=True)
+        self.assertIn(b'Register Page', rv.data)
 
